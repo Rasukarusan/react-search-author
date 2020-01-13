9821ea94b2aad2ef71575b3a24de13c298fb79df
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-01-13 21:28:12 +0900

    必要なパーツだけ配置

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+function InputArea(props) {
+    return (
+        <form class="uk-grid-small" uk-grid>
+            <div class="uk-width-1-2@s">
+                <textarea class="uk-textarea"></textarea>
+            </div>
+        </form>
+    );
+}
+
+function SearchButton(props) {
+    return (
+        <button>検索</button>
+    );
+}
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>著者検索</h1>
+                <InputArea />
+                <SearchButton />
+            </div>
+        );
+    }
+}
+
+class Result extends React.Component {
+    render() {
+        return (
+            <textarea> </textarea>
+        );
+    }
+
+}
+
+function App() {
+    return (
+        <div>
+            <Search />
+            <hr />
+            <Result />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <h1>Hello React!</h1>,
+    <App />,
     document.getElementById('root')
 );
