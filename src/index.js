3c9ca509eeef2fe7e5c3b5f324683459bfbe8f3d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-01-23 23:53:28 +0900

    背景のCSSをカスタマイズ

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 const SEARCH_BUTTON_TEXT_DEFAULT = '検索';
 const SEARCH_BUTTON_TEXT_RUNNING = '検索中...';
@@ -8,6 +8,10 @@ const MESSAGE_NOT_FOUND = '取得できませんでした';
 
 const Container = styled.div`
     text-align: center;
+    background: #4e54c8;
+    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+    width: 100vw;
+    height:100vh;
 `;
 
 const Hr = styled.hr`
@@ -59,11 +63,121 @@ const Button = styled.button`
     `}
 `;
 
+const circleAnimate = keyframes`
+    from {
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
+    }
+    to {
+        transform: translateY(-1500px) rotate(720deg);
+        opacity: 0;
+        border-radius: 50%;
+    }
+`;
+
+const Circle = styled.li`
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: ${circleAnimate} 25s linear infinite;
+    bottom: -150px;
+
+    &:nth-child(1){
+        left: 25%;
+        width: 80px;
+        height: 80px;
+        animation-delay: 0s;
+    }
+
+    &:nth-child(2){
+        left: 10%;
+        width: 20px;
+        height: 20px;
+        animation-delay: 2s;
+        animation-duration: 12s;
+    }
+
+    &:nth-child(3){
+        left: 70%;
+        width: 20px;
+        height: 20px;
+        animation-delay: 4s;
+    }
+
+    &:nth-child(4){
+        left: 40%;
+        width: 60px;
+        height: 60px;
+        animation-delay: 0s;
+        animation-duration: 18s;
+    }
+
+    &:nth-child(5){
+        left: 65%;
+        width: 20px;
+        height: 20px;
+        animation-delay: 0s;
+    }
+
+    &:nth-child(6){
+        left: 75%;
+        width: 110px;
+        height: 110px;
+        animation-delay: 3s;
+    }
+
+    &:nth-child(7){
+        left: 35%;
+        width: 150px;
+        height: 150px;
+        animation-delay: 7s;
+    }
+
+    &:nth-child(8){
+        left: 50%;
+        width: 25px;
+        height: 25px;
+        animation-delay: 15s;
+        animation-duration: 45s;
+    }
+
+    &:nth-child(9){
+        left: 20%;
+        width: 15px;
+        height: 15px;
+        animation-delay: 2s;
+        animation-duration: 35s;
+    }
+
+    &:nth-child(10){
+        left: 85%;
+        width: 150px;
+        height: 150px;
+        animation-delay: 0s;
+        animation-duration: 11s;
+    }
+`;
+
+
 function jsonToResult(json) {
     let item = json.items[0];
     let author = item.volumeInfo.authors ? item.volumeInfo.authors.pop() : MESSAGE_NOT_FOUND;
     let category = item.volumeInfo.categories ? item.volumeInfo.categories.pop() : MESSAGE_NOT_FOUND;
     return '\t' + author + '\t' + category + '\n';
+}
+
+function Circles() {
+    const circles = [...Array(10)].map((_,i) => <Circle key={i}></Circle>);
+    return(
+        <ul>
+            {circles}
+        </ul>
+    );
+
 }
 
 class Search extends React.Component {
@@ -158,6 +272,7 @@ class App extends React.Component {
                     searchButtonDisabled={this.state.searchButtonDisabled}
                     onClick={() => this.handleClick.bind(this)}
                 />
+                <Circles />
                 <Hr />
                 <Result result={this.state.result}/>
             </Container>
